91283191e86f9159f99ac60902747a92ec60ed19
matt <mattclarkeruns@gmail.com>
2021-03-31 21:22:37 -0400

    create gridDisplay

--- src/js/react/gridDisplay.jsx
+++ src/js/react/gridDisplay.jsx
@@ -7,12 +7,34 @@ export default class GridDisplay extends React.Component {
     this.state = {
       lifeGrid: new LifeGrid(10,10)
     }
+
+    this.createGameCell = this.createGameCell.bind(this)
+    this.iterate = this.iterate.bind(this)
+  }
+
+  createGameCell(isAlive) {
+    const cellClass = isAlive ? 'alive' : 'dead'
+    return (
+      <div className={'cell' + ' ' + cellClass}></div>
+    )
   }
 
   render() {
+    const lifeGridRows = this.lifeGrid.map( row => {
+      const cells = row.map(cell => {
+        return this.createGameCell(cell.isAlive)
+      })
+
+      return (
+        <div className='cellRow'>{cells}</div>
+      )
+    })
     return (
       <div>
         <h1> Oh yeah, It is game of life time! </h1>
+        <div className='lifeGrid'>
+          {lifeGridRows}
+        </div>
       </div>
     );
   }
